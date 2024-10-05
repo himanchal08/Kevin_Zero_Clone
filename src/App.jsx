3a7d7ed16a951398bae015037772064dbf832eb6
Ashish Pandey <126936221+Ashish-Pandey0927@users.noreply.github.com>
2024-10-05 17:48:19 +0530

    Add section2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Section2 from './components/Section2'
-import Home from './components/home'
+import Home from './components/Home'
 
 const App = () => {
   return (
